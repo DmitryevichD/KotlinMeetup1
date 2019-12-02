d596e0d4ddbfe1177398f22e04b28276211728d3
Ivan Khramchenkov <i.hramchenkov@itransition.com>
2019-12-02 15:15:47 +0300

    trailing lambdas

---
+++ src/main/kotlin/ru/openbank/meetup/demo/kotlin/trailinglambdas/TrailingLambdasUseCase.kt
@@ -0,0 +1,39 @@
+package ru.openbank.meetup.demo.kotlin.trailinglambdas
+
+import ru.openbank.meetup.demo.kotlin.slide2.OtpDto
+
+
+class TrailingLambdasUseCase {
+
+    private var otpToPhoneLambda: (OtpDto) -> String? = { item -> item.phone }
+
+    private fun invokeTrailingLambda(lambdaParam: (OtpDto) -> String?): String? {
+        return lambdaParam.invoke(OtpDto("5678", "79999999999"))
+    }
+
+    private fun invokeTrailingLambdaWithParam(otpDto: OtpDto, lambdaParam: (OtpDto) -> String?): String? {
+        return lambdaParam.invoke(otpDto)
+    }
+
+    fun lambdaUse() {
+        invokeTrailingLambda(otpToPhoneLambda)
+        invokeTrailingLambda { otpToPhoneLambda.invoke(it) }
+
+        invokeTrailingLambda { it -> it.phone }
+        invokeTrailingLambda { it.phone }
+
+        invokeTrailingLambda { otpDto -> otpDto.phone }
+        invokeTrailingLambda { otpDto: OtpDto -> otpDto.phone }
+
+
+        val otp = OtpDto("5678", "79999999999")
+        invokeTrailingLambdaWithParam(otp, otpToPhoneLambda)
+        invokeTrailingLambdaWithParam(otp) { otpToPhoneLambda.invoke(it) }
+
+        invokeTrailingLambdaWithParam(otp) { it -> it.phone }
+        invokeTrailingLambdaWithParam(otp) { it.phone }
+
+        invokeTrailingLambdaWithParam(otp) { otpDto -> otpDto.phone }
+        invokeTrailingLambdaWithParam(otp) { otpDto: OtpDto -> otpDto.phone }
+    }
+}
